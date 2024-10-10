df4dd99c0df35f38347402349f96cf8ef0a74ca5
BaekEuiHun <euihunbaek@gmail.com>
2024-10-10 14:31:37 +0900

    기본 배열

---
+++ src/collection/array/ArrayMain1.java
@@ -0,0 +1,21 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain1 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        System.out.println(Arrays.toString(arr));
+
+        arr[2] = 10;
+        System.out.println(Arrays.toString(arr));
+        int value = 10;
+
+      
+    }
+}
