85503a70a56e8261626126517ea426823afc098e
BaekEuiHun <euihunbaek@gmail.com>
2024-10-10 19:27:40 +0900

    수정

--- src/collection/list/MyArrayList.java
+++ src/collection/list/MyArrayList.java
@@ -3,7 +3,6 @@ package collection.list;
 import java.util.Arrays;
 
 public class MyArrayList<E> implements MyList<E> {
-
     private static final int DEFAULT_CAPACITY = 5;
     private Object[] elementData;
     private int size = 0;
@@ -20,17 +19,18 @@ public class MyArrayList<E> implements MyList<E> {
     public int size() {
         return size;
     }
+
     @Override
     public void add(E e) {
-        //코드 추가
         if (size == elementData.length) {
             grow();
         }
         elementData[size] = e;
         size++;
     }
+
     @Override
-    public void add(int index, Object e) {
+    public void add(int index, E e) {
         if (size == elementData.length) {
             grow();
         }
@@ -39,51 +39,42 @@ public class MyArrayList<E> implements MyList<E> {
         size++;
     }
 
+    //요소의 마지막부터 index까지 오른쪽으로 밀기
     private void shiftRightFrom(int index) {
-        for (int i = size; i > index; i++) {
+        for (int i = size; i > index; i--) {
             elementData[i] = elementData[i - 1];
         }
     }
 
-    //코드추가
-    private void grow() {
-        int oldCapacity = elementData.length;
-        int newCapacity = oldCapacity * 2;
-//        Object[] newArr = new Object[newCapacity];
-//
-//        for (int i = 0; i < elementData.length; i++) {
-//            newArr[i] = elementData[i];
-//
-//        }
-        Object[] newArr = Arrays.copyOf(elementData, newCapacity);
-        elementData = newArr;
-    }
-    @Override
-    public E remove(int index) {
-        E oldValue = get(index);
-        shiftLeftFrom(index);
-
-        size--;
-        elementData[size] = null;
-        return oldValue;
-    }
-
-    private void shiftLeftFrom(int index) {
-        for (int i = index; i < size - 1; i++) {
-            elementData[i] = elementData[i + 1];
-        }
-    }
     @Override
     @SuppressWarnings("unchecked")
     public E get(int index) {
         return (E) elementData[index];
     }
+
     @Override
     public E set(int index, E element) {
         E oldValue = get(index);
         elementData[index] = element;
         return oldValue;
     }
+
+    @Override
+    public E remove(int index) {
+        E oldValue = get(index);
+        shiftLeftFrom(index);
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    //요소의 index부터 마지막까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+    }
+
     @Override
     public int indexOf(E o) {
         for (int i = 0; i < size; i++) {
@@ -94,11 +85,15 @@ public class MyArrayList<E> implements MyList<E> {
         return -1;
     }
 
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
     @Override
     public String toString() {
         return Arrays.toString(Arrays.copyOf(elementData, size)) + " size=" +
             size + ", capacity=" + elementData.length;
     }
 }
-
-
