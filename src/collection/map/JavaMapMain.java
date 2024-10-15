a9a86febdb3eee52d80c70b31ab935a809a9ee6b
BaekEuiHun <euihunbaek@gmail.com>
2024-10-15 15:36:12 +0900

    HashMap,LinkedHashMap,TreeMap

--- src/collection/map/JavaMapMain.java
+++ src/collection/map/JavaMapMain.java
@@ -20,7 +20,7 @@ public class JavaMapMain {
         Iterator<String> iterator = keySet.iterator();
         while (iterator.hasNext()) {
             String key = iterator.next();
-            System.out.print(key + "=" + map.get(key) + " ");
+            System.out.print (key + "=" + map.get(key) + " ");
         }
         System.out.println();
 
